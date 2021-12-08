e4479e60f84fadf2cdab238b36e371b5c456132f
Avanish65 <avanishsharmaramsuresh@gmail.com>
2021-12-09 00:50:09 +0530

    changes made from lappy

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="\<br\>\<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    if (principal <=0) {
+        alert("Value should be greater than 0");
+            document.getElementById("principal").focus();
+    }
+    else {
+    document.getElementById("result").innerHTML="\<br\>\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    }
 
 }
 function updateRate() 
